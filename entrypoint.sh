c7c9dac94f065e8e6b102f58b4329c1536780323
mike dupont <mike.dupont@introspector.local>
2023-10-05 16:12:37 -0400

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,7 @@ set -e
 
 # skip if no /revert
 echo "Checking if contains '/revert' command..."
-(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -E "/revert") || exit 78
-
-# skip if not a PR
-echo "Checking if a PR command..."
-(jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
-
-# get the SHA to revert
-COMMIT_TO_REVERT=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | cut -c 9-)
+(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -E "/agent-action") || exit 78
 
 if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
 	echo "This is not a new comment event!"
@@ -37,18 +30,21 @@ pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
 HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
 HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
 
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-git config --global user.email "revert@github.com"
-git config --global user.name "GitHub Revert Action"
+#git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+#git config --global user.email "revert@github.com"
+#git config --global user.name "GitHub Revert Action"
 
 set -o xtrace
 
-git fetch origin $HEAD_BRANCH
+#git fetch origin $HEAD_BRANCH
 
 # do the revert
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+#git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
 # check commit exists
-git cat-file -t $COMMIT_TO_REVERT
-git revert $COMMIT_TO_REVERT --no-edit
-git push origin $HEAD_BRANCH
+#git cat-file -t $COMMIT_TO_REVERT
+#git revert $COMMIT_TO_REVERT --no-edit
+#git push origin $HEAD_BRANCH
+
+OPENAI_API_BASE=http://host.docker.internal:8080/v1
+docker run h4ckermike/autogpt
